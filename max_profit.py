73350aaacbe151d6b480c2938d9d30c27a530245
occuzy <occuzy@gmail.com>
2024-09-13 10:27:57 +0900

    i got two company

--- max_profit.py
+++ max_profit.py
@@ -1,9 +1,9 @@
 def maxProfit_bruteforce (prices):
-   max_price = 0
+    max_price = 0
+    print("Make Money")
+    for i, price in enumerate(prices):
+        for j in range(i, len(prices)):
+            max_price = max(prices[j] - price, max_price)
 
-   for i, price in enumerate(prices):
-       for j in range(i, len(prices)):
-           max_price = max(prices[j] - price, max_price)
-
-   return
+    return
 
